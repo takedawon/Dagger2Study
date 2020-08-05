52977454bfdb784ce3671d103257b2660e27c69f
takedawon <takedawon@naver.com>
2020-08-05 17:01:14 +0900

    NetworkModule 작성

---
+++ app/src/main/java/com/lanic/myapplication/NetworkModule.kt
@@ -0,0 +1,44 @@
+package com.lanic.myapplication
+
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+class NetworkModule {
+
+    @Provides
+    @Named("unauthorized")
+    @Singleton
+    private fun provideUnauthorizedOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    @Provides
+    @Named("authorized")
+    @Singleton
+    private fun provideAuthorizedOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+        authInterceptor: AuthInterceptor) : OkHttpClient
+    = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .addInterceptor(authInterceptor)
+        .build()
+
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    fun provideAuthInterceptor(provider: AuthTokenProvider): AuthInterceptor {
+        val token = provider.token ?: throw IllegalStateException("authToken cannot be null")
+        return AuthIntercpetor(token)
+    }
+}
